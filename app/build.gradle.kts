22801d120526c376cda5dac634db9d8ecd95df00
Adzer2605 <iqbalabdullah2605@gmail.com>
2026-04-14 14:33:20 +0700

    Add Dependencies

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -18,6 +18,7 @@ plugins {
     id("com.android.application")
     id("org.jetbrains.kotlin.android")
     id("org.jetbrains.kotlin.plugin.compose")
+    id("com.google.devtools.ksp")
 }
 
 android {
@@ -78,4 +79,11 @@ dependencies {
     debugImplementation("androidx.compose.ui:ui-tooling")
 
     testImplementation("junit:junit:4.13.2")
+
+    val room_version = "2.6.1"
+    implementation("androidx.room:room-runtime:2.8.4")
+    implementation("androidx.room:room-ktx:2.8.4")
+    ksp("androidx.room:room-compiler:2.8.4")
+
+
 }
